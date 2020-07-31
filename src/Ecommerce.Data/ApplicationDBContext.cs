e663727f973866a86dd5b8efd61e9aeabba5d19f
Renan Barbosa <renan.vinicius@sga.pucminas.br>
2020-07-30 21:00:46 -0300

    Configuração do Entity Framework

---
+++ src/Ecommerce.Data/ApplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Ecommerce.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Client> Client { get; set; }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<OrderItem> OrderItem { get; set; }
+        public DbSet<Product> Product { get; set; }
+    }
+}
