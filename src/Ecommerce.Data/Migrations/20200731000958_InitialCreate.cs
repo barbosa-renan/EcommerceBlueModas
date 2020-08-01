4164cabd248ea794ee26847734e8eec31995f7e6
Renan Barbosa <renan.vinicius@sga.pucminas.br>
2020-08-01 08:59:05 -0300

    Página de listagem dos produtos

---
+++ src/Ecommerce.Data/Migrations/20200731000958_InitialCreate.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ecommerce.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Client",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Phone = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Client", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    ImagePath = table.Column<string>(nullable: true),
+                    UnitPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Order",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClientId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Order", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Order_Client_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Client",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderItem",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderId = table.Column<int>(nullable: false),
+                    ProductId = table.Column<int>(nullable: true),
+                    Quantity = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderItem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OrderItem_Order_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Order",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderItem_Product_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Product",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Order_ClientId",
+                table: "Order",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItem_OrderId",
+                table: "OrderItem",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItem_ProductId",
+                table: "OrderItem",
+                column: "ProductId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderItem");
+
+            migrationBuilder.DropTable(
+                name: "Order");
+
+            migrationBuilder.DropTable(
+                name: "Product");
+
+            migrationBuilder.DropTable(
+                name: "Client");
+        }
+    }
+}
--- src/Ecommerce.UI.Web/Controllers/HomeController.cs
+++ src/Ecommerce.UI.Web/Controllers/HomeController.cs
@@ -1,37 +1,100 @@
-﻿using System;
+﻿using Ecommerce.Data;
+using Ecommerce.Domain;
+using Ecommerce.UI.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using Ecommerce.UI.Web.Models;
 
 namespace Ecommerce.UI.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDBContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDBContext context)
         {
             _logger = logger;
+            _context = context;
+
         }
 
         public IActionResult Index()
         {
-            return View();
-        }
+            var products = _context.Product.ToList();
 
-        public IActionResult Privacy()
-        {
-            return View();
+            if (products.Count == 0)
+                PopulateDatabaseToTest();
+
+            return View(products);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        /// <summary>
+        /// Create a list of products and insert it in the database for testing.
+        /// </summary>
+        private void PopulateDatabaseToTest()
+        {
+            var products = new List<Product> {
+                new Product
+                {
+                    Name = "Camisa Básica",
+                    ImagePath = "products/shirts/01.jpg",
+                    UnitPrice = 29.90m
+               },
+               new Product
+                {
+                    Name = "Camisa Polo Madri",
+                    ImagePath = "products/shirts/02.jpg",
+                    UnitPrice = 35.00m
+               },
+               new Product
+                {
+                    Name = "Camisa Social Slim Fit",
+                    ImagePath = "products/shirts/03.jpg",
+                    UnitPrice = 59.00m
+               },
+               new Product
+                {
+                    Name = "Camisa Social Feminina",
+                    ImagePath = "products/shirts/04.jpg",
+                    UnitPrice = 39.90m
+               },
+               new Product
+                {
+                    Name = "Calça Jeans Masculina",
+                    ImagePath = "products/pants/01.jpg",
+                    UnitPrice = 110.50m
+               },
+               new Product
+                {
+                    Name = "Calça Jeans Feminina",
+                    ImagePath = "products/pants/02.jpg",
+                    UnitPrice = 139.90m
+               },
+               new Product
+                {
+                    Name = "Calça Social Feminina",
+                    ImagePath = "products/pants/03.jpg",
+                    UnitPrice = 74.00m
+               },
+               new Product
+                {
+                    Name = "Calça Social Masculina",
+                    ImagePath = "products/pants/04.jpg",
+                    UnitPrice = 74.00m
+               }
+            };
+
+            _context.AddRange(products);
+            _context.SaveChanges();
         }
     }
 }
