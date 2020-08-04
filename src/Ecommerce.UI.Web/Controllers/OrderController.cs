23bce82a475ad95e7130571952bf34237ae57649
Renan Barbosa <renan.vinicius@sga.pucminas.br>
2020-08-03 21:42:00 -0300

    Inclusão do cesto de pedidos no cookies

--- src/Ecommerce.UI.Web/Controllers/OrderController.cs
+++ src/Ecommerce.UI.Web/Controllers/OrderController.cs
@@ -1,7 +1,10 @@
 ﻿using Ecommerce.Data;
 using Ecommerce.Domain;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -9,6 +12,7 @@ namespace Ecommerce.UI.Web.Controllers
 {
     public class OrderController : Controller
     {
+        private const string _cookieName = "Order";
         private readonly ILogger<HomeController> _logger;
         private readonly ApplicationDBContext _context;
 
@@ -20,10 +24,13 @@ namespace Ecommerce.UI.Web.Controllers
 
         public IActionResult Index()
         {
-            //TODO: Recuperar order armazenado nos cookies
             return View(GetCurrentOrder());
         }
 
+        /// <summary>
+        /// Adiciona um produto ao carrinho com base no id informado.
+        /// </summary>
+        /// <param name="productId">Primary key do produto</param>
         [HttpPost]
         [Route("add-to-cart")]
         public void AddToCart([FromBody] int productId)
@@ -56,29 +63,52 @@ namespace Ecommerce.UI.Web.Controllers
 
                 order.Items.Add(orderItem);
 
-                //TODO: incluir order em cookie e atualizar contador do carrinho
+                AddToCookieData(order);
             }
         }
 
+        /// <summary>
+        /// Recupera os dados de um pedido se tiver um cookie já criado.
+        /// </summary>
+        /// <returns>Pedido do tipo order</returns>
         private Order GetCurrentOrder()
         {
-            //TODO: Recuperar order armazenado em cookie
             Order order = new Order();
             order.Items = new List<OrderItem>();
 
-            foreach (Product product in _context.Product.ToList())
-            {
-                OrderItem item = new OrderItem
-                {
-                    OrderId = order.Id,
-                    Product = product,
-                    Quantity = 1
-                };
+            string cookie = HttpContext.Request.Cookies[_cookieName];
 
-                order.Items.Add(item);
+            if (cookie != null)
+            {
+                order = JsonConvert.DeserializeObject<Order>(cookie);
             }
 
             return order;
+        }
+
+        /// <summary>
+        /// Adiciona o pedido ao cookie
+        /// </summary>
+        /// <param name="order">Pedido com os produtos selecionados</param>
+        private void AddToCookieData(Order order)
+        {
+            CookieOptions options = CreateOptions();
+
+            string data = JsonConvert.SerializeObject(order);
+
+            HttpContext.Response.Cookies.Append(_cookieName, data, options);
+        }
+
+        /// <summary>
+        /// Configura as opções do cookie.
+        /// </summary>
+        private CookieOptions CreateOptions()
+        {
+            return new CookieOptions
+            {
+                Secure = true,
+                MaxAge = TimeSpan.FromDays(30)
+            };
         }
     }
 }
